755d76a24b237fe352a9edf37c9b19d5af492c88
Dane Bramble <Dane@MacBook-Pro-3.local>
2018-01-30 17:31:38 -0500

    Print function working, now on to next_gen

--- life.c
+++ life.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "life.h"
 
 void create_universe(int boardwidth, int boardheight){
     boardw = boardwidth;
     boardh = boardheight;
 	world = malloc((boardwidth*boardheight)*sizeof(char));
-    srand(time(null));
     
-    for (i = 0; i < (boardwidth*boardheight);i++){
+    for (int i = 0; i < (boardwidth*boardheight);i++){
         world[i] = '-';
-        random = rand() % (boardwicth*boardheight);
-        world[random] = '*';
+        if (i % 2 == 0){
+        	world[i] = '*';
+        }
     }
     print_world();
 }
@@ -21,33 +22,46 @@ void next_gen(int gencount){
 }
 
 void print_world(){
-    for (i = 0; i < (boardw*boardh); i ++){
-        if (i % board = 0) {
-            printf(%c, world[i]);
+	printf("\n\nCell Key:\n   Alive: *\n   Dead: O\n   Empty: -\n\n");
+    for (int i = 1; i <= (boardw*boardh); i ++){
+        if ((i % boardw == 0)) {
+            printf("%c\n",world[i - 1]);
         } else {
-            printf(%c\n, world[i]);
+            printf("%c",world[i - 1]);
         }
     }
     next_action();
 }
 
 void next_action(){
-	char option[10];
-	printf("Next Move? Options:\n save,\n quit,\n continue one generation (continue),\n or continue 'insert number' generations (more)?";
-	scanf(%s, &option);
-	switch(option){
-		case 'continue':
-			next_gen(1);
-		case 'more':
-			int option2;
-			printf("How many generations do you want to skip?\n Insert Number:"
-			scanf(%d, &option2);
-			next_gen(option2);
-		case 'save':
-		case 'load':
-		case 'quit':
-		default :
-			printf("I'm sorry, that's not an option");
-			next_action();
+	char option[50];
+	printf("\nNext Move?\nOptions:\n   save,\n   quit,\n   continue one generation (continue),\n   continue many generations (more)\n");
+	scanf("%s", option);
+	if (strcmp(option, "continue") == 0){
+		next_gen(1);
+	}
+	else if (strcmp(option, "more") == 0){
+		int option2 = 0;
+		printf("How many generations do you want to skip?\nInsert Number:");
+		scanf("%d", &option2);
+		next_gen(option2);
+	}
+	else if (strcmp(option, "save") == 0){
+	
+	}
+	else if (strcmp(option, "load") == 0){
+
+	}
+	else if (strcmp(option, "quit") == 0){
+		free(world);
+	}
+	else{
+		printf("Please enter a valid option");
+		next_action();
 	}
 }
+
+
+int main( int argc, char* argv[]){
+	create_universe(25,25);
+} 	
